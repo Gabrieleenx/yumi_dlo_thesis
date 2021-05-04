3c884675aaae4bb284f258865cccc25ade3ef6ca
Gabriel <gabwal@student.chalmers.se>
2021-05-04 15:47:26 +0200

    Data logging

--- path_planner/src/constraintsCheck.py
+++ path_planner/src/constraintsCheck.py
@@ -5,12 +5,16 @@ import tf
 import utils, utilsSolve
 
 
-def check(task):
+def check(task, logger):
+
     instruction = 0
     mode = task.mode
+    logger.appendPathplannerState(data='Checking trajectory, mode = ' + mode)
+
     within = utilsSolve.checkTaskWithinReach(task=task)
     if not within:
         print("trajectories not in reach")
+        logger.appendPathplannerState(data='Problem detected, trajectories are outside of reach')
         instruction = 1
 
     if mode == 'individual': # assumption, individual used for picking up cable 
@@ -18,21 +22,25 @@ def check(task):
         trajClear = utilsSolve.checkCloseToFixtures(task=task)
         if not trajClear:
             print("trajectories to close to fixture")
+            logger.appendPathplannerState(data='Problem detected, trajectories are to close to fixture')
             instruction = 1
 
         trajClear = utilsSolve.checkIfTrajectoriesPassToClose(task=task)
         if not trajClear:
             print("trajectories pass to close ")
+            logger.appendPathplannerState(data='Problem detected, trajectories pass to close to each other')
             instruction = 1
         
         armNotCross = utilsSolve.checkIfNotLeftRightArmCross(task=task)
         if not armNotCross:
             print('Crossing individual ')
+            logger.appendPathplannerState(data='Problem detected, trajectories cross to far on y axis')
             instruction = 1
 
         overRotation = utilsSolve.checkOverRotation(task=task)
         if not overRotation:
             print('Overrotation individual ')
+            logger.appendPathplannerState(data='Problem detected, sqeuence of orientations are outside of limits')
             instruction = 3
 
     elif mode == 'combined':
@@ -40,6 +48,7 @@ def check(task):
         armNotCross = utilsSolve.checkIfNotLeftRightArmCross(task=task)
         if not armNotCross:
             print('Crossing combined ')
+            logger.appendPathplannerState(data='Problem detected, trajectories cross to far on y axis')
             instruction = 1
         
     return instruction
--- path_planner/src/pathPlannerMaster.py
+++ path_planner/src/pathPlannerMaster.py
@@ -6,12 +6,12 @@ from sensor_msgs.msg import PointCloud
 from std_msgs.msg import Int64
 import tf
 import numpy as np
-import utils, constraintsCheck, solveRerouting
+import utils, constraintsCheck, solveRerouting, dataLogger
 import tasks
 import threading
 
 class PathPlanner(object):
-    def __init__(self, listOfObjects, listOfTasks):
+    def __init__(self, listOfObjects, listOfTasks, logger):
 
         # initial enviorment and planned tasks
         self.map = listOfObjects
@@ -39,6 +39,8 @@ class PathPlanner(object):
         self.mtx_jointPos = threading.Lock()
         # ros publisher for trajectory 
         self.pub = rospy.Publisher('/Trajectroy', Trajectory_msg, queue_size=1)
+        # logger
+        self.logger = logger
 
     def callback(self, data):
         # update DLO estimation
@@ -52,6 +54,7 @@ class PathPlanner(object):
             DLOPoints[i,2] = data.points[i].z
         # update DLO threadsafe
         self.mtx_spr.acquire()
+        self.logger.appendDLOPoints(data=DLOPoints)
         self.DLO.update(points=DLOPoints)
         self.mtx_spr.release()
 
@@ -69,6 +72,7 @@ class PathPlanner(object):
     def genetateNewTrajectory(self, task):
         # alway tries to generate new trajectory in normal state
         print('Generate new trajectory = ', self.currentTask)                 
+        self.logger.appendPathplannerState(data='Generate new trajectory, task = ' + str(self.currentTask))
 
         task = self.tasks[self.currentTask]
         task.updateAndTrackProgress(map_=self.map,\
@@ -80,9 +84,10 @@ class PathPlanner(object):
         # generates message
         msg = task.getMsg() 
         # Check for imposible solutions
-        self.instruction = constraintsCheck.check(task=task)
+        self.instruction = constraintsCheck.check(task=task, logger=self.logger)
         # if problem detected
         if self.instruction == 1:
+            self.logger.appendPathplannerState(data='Hold position sent')
             self.holdPosition.resetTask()
             self.holdPosition.updateAndTrackProgress(map_=self.map,\
                                             DLO=self.DLO,\
@@ -92,25 +97,30 @@ class PathPlanner(object):
                                             jointPosition=self.jointPosition)   
             msg = self.holdPosition.getMsg()
             self.pub.publish(msg)
+            # logger
+            self.logger.appendMsgSent(data=msg)
             nonValidSolution = 1
             numAttempts = 0 # keep track of how many attempts of findin valid solution
             
             while nonValidSolution == 1: 
                 print('Searching for solution')
-
                 numAttempts += 1
                 if numAttempts > 3:
                     print('Number of attempts exceded 3')
                     print('Non recoverable problem detected in path planning!')
                     self.instruction = 2
+                    self.logger.appendPathplannerState(data='Number of attempts exceded, non solvable state')
                     return
+
+                self.logger.appendPathplannerState(data='Searching for solution, attempt = '+ str(numAttempts))
                 # setup solver
                 self.solve.updateInit(task=task)
                 # solve optim problem, evolution based stochastic solver. 
                 individual = self.solve.solve(populationSize=100, numGenerations=20)
-
+                self.logger.appendSolutionIndividual(data=individual)
                 if not (individual.pickupRightValid or individual.pickupLeftValid or individual.combinedValid):
                     print('Non valid solution')
+                    self.logger.appendPathplannerState(data='Non valid solution, from evaluation')
                     continue
 
                 # setup new task 
@@ -131,7 +141,7 @@ class PathPlanner(object):
                 # replace message from new task with solution
                 msg = task_.getMsg()
                 # Check for imposible solutions
-                nonValidSolution = constraintsCheck.check(task=task_)
+                nonValidSolution = constraintsCheck.check(task=task_, logger=self.logger)
                 if nonValidSolution == 3:
                     self.instruction = 3
             task = task_
@@ -147,11 +157,14 @@ class PathPlanner(object):
                                             jointPosition=self.jointPosition)   
             msg = self.holdPosition.getMsg()
             self.pub.publish(msg)
+            # logger
+            self.logger.appendMsgSent(data=msg)
             return
 
         if self.instruction == 3:
             # reset orientation 
             print('reseting orientation')
+            self.logger.appendPathplannerState(data='Reset orientation')
             self.resetOrientation.resetTask()     
             self.resetOrientation.updateAndTrackProgress(map_=self.map,\
                                             DLO=self.DLO,\
@@ -161,6 +174,7 @@ class PathPlanner(object):
                                             jointPosition=self.jointPosition)   
             msg = self.resetOrientation.getMsg()
         self.pub.publish(msg)
+        self.logger.appendMsgSent(data=msg)
 
     def update(self):
         # Main function beeing called in a loop, generates trajcectories and checks execution 
@@ -174,6 +188,7 @@ class PathPlanner(object):
         (posLeft, orientationLeft) = self.tfListener.lookupTransform('/yumi_base_link', '/yumi_gripp_l', rospy.Time(0))
         self.gripperRight.update(position=posRight, orientation=orientationRight)
         self.gripperLeft.update(position=posLeft, orientation=orientationLeft)
+        
         
         # threadsafe for DLO and subtasks
         self.mtx_spr.acquire()
@@ -193,6 +208,12 @@ class PathPlanner(object):
             return
 
         self.mtx_jointPos.acquire()
+
+        #logger
+        self.logger.appendGripperPose(data=[posRight, orientationRight, posLeft, orientationLeft])
+        self.logger.appendJointPosition(data=np.copy(self.jointPosition))
+        self.logger.appendControllerState(data=self.currentSubTask)
+        self.logger.appendCurrentTask(data=self.currentTask)
         # if a new trajectory is called for
         if task.getNewTrajectory() == 1:
             self.genetateNewTrajectory(task=task)
@@ -236,6 +257,9 @@ def main():
     tfListener = tf.TransformListener()
     # sleep for listener to have time to pick up the transformes 
     rospy.sleep(0.5)
+    
+    # Logger
+    logger = dataLogger.Logger()
 
     # add existing fixtures in list
     fixtureList = ['/Fixture1','/Fixture2','/Fixture3','/Fixture4']
@@ -250,6 +274,9 @@ def main():
                                         fixtureHeight=0.06,\
                                         fixtureRadius=0.06)
             listOfObjects.extend([obj])
+            logger.appendPathplannerState(data='Adding Fixture ' + str(i))
+            logger.appendFixturesObj(data=obj)
+
         except:
             break
 
@@ -261,8 +288,10 @@ def main():
         grabCable = tasks.GrabCable(targetFixture=i, previousFixture=i-1, cableSlack=slackList[i])
         clippIntoFixture = tasks.ClippIntoFixture(targetFixture=i, previousFixture=i-1, cableSlack=slackList[i])
         listOfTasks.extend([grabCable, clippIntoFixture])
+        logger.appendPathplannerState(data='Adding tasks, grabCable')
+        logger.appendPathplannerState(data='Adding tasks, clippIntoFixture')
 
-    pathPlanner = PathPlanner(listOfObjects=listOfObjects, listOfTasks=listOfTasks)
+    pathPlanner = PathPlanner(listOfObjects=listOfObjects, listOfTasks=listOfTasks, logger=logger)
 
     # Ros subscribers 
     rospy.Subscriber("/spr/dlo_estimation", PointCloud, pathPlanner.callback, queue_size=2)
@@ -282,6 +311,10 @@ def main():
 
         rate.sleep()
 
+    print('Shuting down')
+    # save data
+    dataLogger.saveObjectPickle(fileName='test1.obj', object_=logger)
+    print('Done!')
 
 if __name__ == '__main__':
     main()
